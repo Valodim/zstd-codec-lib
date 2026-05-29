0a3a6e361d413c9cc02b58699f35360ad2922050
Vincent Breitmoser <look@my.amazin.horse>
2026-05-29 22:54:07 +0200

    adjust shell.nix for playwright

--- shell.nix
+++ shell.nix
@@ -1,17 +1,25 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-  packages = with pkgs; [
-    nodejs_22
-    yarn
-    bun
-    llvmPackages_21.clang-unwrapped
-    lld
-    binaryen
-    gnumake
-    zopfli
-  ];
+let
+  pkgs = import <nixpkgs> {};
+  playwrightPkgs = import (builtins.fetchTarball {
+    name = "nixos-unstable-20250-09-23";
+    url = "https://github.com/nixos/nixpkgs/archive/b2a3852bd078e68dd2b3dfa8c00c67af1f0a7d20.tar.gz";
+    sha256 = "sha256:0lgg0bw6gnaa0sg45da65qzmhwrwjj7gsq2scfq8wcbv0bnc9xb9";
+  }) {};
+in
+  pkgs.mkShell {
+    packages = with pkgs; [
+      nodejs_22
+      yarn
+      bun
+      llvmPackages_21.clang-unwrapped
+      llvmPackages_21.lld
+      binaryen
+      gnumake
+      zopfli
+    ];
 
-  shellHook = ''
-    export LLVM_DIR=${pkgs.llvmPackages_21.clang-unwrapped}
-  '';
-}
+    LLVM_DIR = pkgs.llvmPackages_21.clang-unwrapped;
+
+    # for driving playwright
+    PLAYWRIGHT_BROWSERS_PATH = playwrightPkgs.playwright-driver.browsers;
+  }
