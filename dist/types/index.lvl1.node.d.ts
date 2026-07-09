9d0370090414b450bd4cd3edefb26843d688c805
Vincent Breitmoser <look@my.amazin.horse>
2026-07-09 22:31:53 +0200

    drop outdated artifacts

--- dist/types/index.lvl1.node.d.ts
+++
@@ -1,4 +0,0 @@
-export { createDecoder, decompress, decompressStream, decompressSync, setupZstdDecoder, ZstdDecoder, ZstdDecompressionStream, } from './shared.js';
-export { compress, compressSync, createEncoder, setupZstdCodec, ZstdCompressionStream, ZstdEncoder, } from './encoder-shared.js';
-export type { DecoderOptions, EncoderOptions, CodecOptions, StreamResult } from './types.js';
-//# sourceMappingURL=index.lvl1.node.d.ts.map
